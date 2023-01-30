be86100c6c378442f6d27365d4758c56aa9bd8f4
John Doe <supergoblin08@gmail.com>
2023-01-30 18:13:56 +0300

    попытался сделать очень фанни генератор айкбю

--- src/main.cpp
+++ src/main.cpp
@@ -9,34 +9,10 @@
 using namespace std;
 
 int main() {
-  cout << "Введите имя вводного файла с IQ или напишите R(r) чтобы случайно его сгенерировать: ";
-  vector<Person> persons;
-  string choice;
-  cin >> choice;
-
-  if (choice == "R" || choice == "r") {
-    cout << endl << "Введите имя файла, в который значеня будут сохранены: ";
-
-    string name;
-    cin >> name;
-
-    ofstream random_file;
-    random_file.open(name);
-    for (int i = 0; i < 100; i++){
-      int num = (rand() % 140) - 20;
-      cout << num << " ";
-      random_file << num << "\n";
-    }
-    random_file.close();
-    
-    cout << endl << "Случайные числа сохранены в файле " << name << "." << endl;
-    choice = name;
+  Person population[100];
+  for (int i = 0; i < 100; i++){
+    population[i] = Person((rand() % 140) - 20);
   }
-
-  // Открытие файла
-
-  // ofstream citizens(choice);
-  cout << "Выбран файл " << choice << "." << endl;
   // Конец программы.
   return 0;
 }
